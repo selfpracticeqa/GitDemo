4c21c2ef0378781b3ae43292c97c56075560ba2d
Niaz <selfpracticeqa@gmail.com>
2020-04-24 08:15:58 -0400

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest 
+{
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("Android Apps");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOS Apps");
+	}
+}
---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest 
+{
+	@Test
+	public void postJira()
+	{
+		System.out.println("Post jira");
+	}
+
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("Delete Twitter");
+	}
+}
+
+
